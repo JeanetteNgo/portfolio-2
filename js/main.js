de2f4523536a805a87af4dc4f9e9b03133c44b0a
JeanetteNgo <jeanettengo@u.nus.edu>
2021-05-25 17:40:02 +0800

    Updated portfolio page; shopee case study
    Updated general layout, responsive layout, images, resume

--- js/main.js
+++ js/main.js
@@ -61,3 +61,31 @@ function openTab(event, tabName) {
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
   }
+
+// PORTFOLIO SIDEBAR FILTER
+////////////////////////////////////////////////////////////////////////////////
+const categoryTitle = document.querySelectorAll(".category-title");
+const allCategoryPosts = document.querySelectorAll(".all");
+
+for (let i = 0; i < categoryTitle.length; i++){
+	categoryTitle[i].addEventListener("click", filterPosts.bind(this, categoryTitle[i]));
+}
+
+function filterPosts(item){
+	changeActivePosition(item);
+	for (let i = 0; i < allCategoryPosts.length; i++){
+		if(allCategoryPosts[i].classList.contains
+			(item.attributes.id.value)){
+				allCategoryPosts[i].style.display = "block";
+			} else {
+				allCategoryPosts[i].style.display = "none";
+			}
+	}
+};
+
+function changeActivePosition(activeItem){
+	for (let i = 0; i < categoryTitle.length; i++){
+		categoryTitle[i].classList.remove("category-active");
+	}
+	activeItem.classList.add("category-active");
+}
